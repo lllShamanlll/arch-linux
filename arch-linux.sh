34ff9eb184b833ed70dbfcf5375882efa3044c57
lllShamanlll <lllShamanlll@gmail.com>
2016-11-03 01:51:07 +0300

    sda1 made bootable

--- arch-linux.sh
+++ arch-linux.sh
@@ -11,6 +11,7 @@ echo "Creating paritions..."
 # /     - /dev/sda2
 parted -s /dev/sda mktable msdos
 parted -s /dev/sda mkpart primary 0% 100m
+parted -s set 1 boot on
 parted -s /dev/sda mkpart primary 100m 100%
 
 echo "Formating paritions..."
