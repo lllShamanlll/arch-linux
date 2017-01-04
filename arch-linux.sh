42e227dcf81a9a0f050e9bd4302d48c36c0e1db4
lllShamanlll <lllShamanlll@gmail.com>
2017-01-04 03:03:35 +0300

    Now it works.

--- arch-linux.sh
+++ arch-linux.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 echo "Arch Linux installation begins."
 
 echo "Looking for internet connection..."
@@ -7,26 +10,23 @@ if [[ ! $(curl -I -L http://www.google.com/ | grep HTTP | tail -n 1) =~ "200 OK"
 fi
 
 echo "Creating paritions..."
-# /boot - /dev/sda1
-# /     - /dev/sda2
 parted -s /dev/sda mktable msdos
-parted -s /dev/sda mkpart primary 0% 100m
+parted -s /dev/sda mkpart primary 0% 100%
 parted -s /dev/sda set 1 boot on
-parted -s /dev/sda mkpart primary 100m 100%
 
 echo "Formating paritions..."
 mkfs.ext2 /dev/sda1
-mkfs.btrfs /dev/sda2
 
 echo "Mounting paritions..."
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/sda1 /mnt
 
 echo "Installing base packages..."
 pacstrap /mnt base base-devel
 
-# chroot
+#
+# BEGIN chroot
+#
+
 arch-chroot /mnt /bin/bash << EOF
 
 echo "Initilizing locale..."
@@ -36,21 +36,11 @@ echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
 echo "Initilizing timezome..."
-# set initial timezone to Europe/Moscow
 ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc --utc
 
-echo "Updating packages..."
-pacman -Sy
-
 echo "Configuring hostname..."
 echo "archlinux-$(date -I)" > /etc/hostname
-
-echo "Configuring network..."
-systemctl enable dhcpcd@enp0s3.service
-
-echo "Installing additional packages..."
-pacman -S -q sudo bash-completion
 
 echo "Configuring root password..."
 echo root:root | chpasswd
@@ -58,18 +48,37 @@ echo root:root | chpasswd
 echo "Running mkinitcpio..."
 mkinitcpio -p linux
 
+echo "Updating packages..."
+pacman -Sy
+
 echo "Configuring bootloader..."
-pacman -Sq grub os-prober
+pacman -S --noconfirm grub os-prober
 grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
+echo "Configuring network..."
+echo "[Match]" >> /etc/systemd/network/host-only.network
+echo "Name=enp*" >> /etc/systemd/network/host-only.network
+echo "" >> /etc/systemd/network/host-only.network
+echo "[Network]" >> /etc/systemd/network/host-only.network
+echo "DHCP=ipv4" >> /etc/systemd/network/host-only.network
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+
+echo "Installing additional packages..."
+pacman -S --noconfirm sudo bash-completion
+
 EOF
+
+#
+# END chroot
+#
 
 echo "Generating fstab..."
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
 echo "Unmounting paritions..."
-umount /mnt/{boot,}
+umount /mnt
 
 echo "Arch Linux installation successfully finished."
 echo "Unmount the CD image from the VM, then reboot."
