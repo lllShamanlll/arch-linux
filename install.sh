f27c2f6e5a09671b058de921a9dc80b546cb1640
lllShamanlll <lllShamanlll@gmail.com>
2017-01-07 11:50:35 +0300

    \$ fix

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ pacman -S --noconfirm xorg-server xorg-xinit
 pacman -S --noconfirm emacs
 xinitrcsep="# start some nice programs"
 xinitrcpath="/etc/X11/xinit/xinitrc"
-sed -n "/$xinitrcsep/i exec emacs" $xinitrcpath | sed -n "/$xinitrcsep/q;p" > $xinitrcpath
+sed -n "/\$xinitrcsep/i exec emacs" \$xinitrcpath | sed -n "/\$xinitrcsep/q;p" > \$xinitrcpath
 
 # SLiM
 pacman -S --noconfirm slim
